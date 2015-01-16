77cd69229e1aa7a43d0eedb3127d12987d17875c
mikebarksdale <mike.barksdale@gmail.com>
2015-01-16 14:50:14 -0500

    included quick unit test

---
+++ SandboxPolicy.Tests/RepoTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SandboxPolicy.Models;
+using SandboxPolicy.Models.Fakes;
+using SandboxPolicy.Services;
+using SandboxPolicy.Services.Service;
+
+namespace SandboxPolicy.Tests
+{
+    [TestClass]
+    public class RepoTests
+    {
+        protected static IUnityContainer Container;
+        protected static IPolicyServiceFactory PolicyServiceFactory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Container = new UnityContainer();
+            Container.RegisterType<IPolicyServiceFactory, PolicyServiceFactory>();
+
+            PolicyServiceFactory = Container.Resolve<IPolicyServiceFactory>();
+        }
+
+        [TestMethod]
+        public void CreatePolicyDbPass()
+        {
+            var policyModel = new StubIPolicyModel
+            {
+                EffectiveDateGet = () => new DateTime(2014, 12, 25),
+                ExpirationDateGet = () => new DateTime(2015, 12, 25),
+                InsuredsGet = () => new List<IInsuredModel>(),
+                CoveragesGet = () => new List<ICoverageModel>(),
+                UserGet = () => "testmb"
+            };
+
+            var service = PolicyServiceFactory.GetService(typeof (PolicyDbService));
+            var policy = service.CreatePolicy(policyModel);
+
+            Assert.IsNotNull(policy.PolicyNumber);
+            Assert.IsTrue(policy.PolicyId > 0);
+            Assert.IsTrue(policy.Status.Equals("Quote", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [TestMethod]
+        public void CreatePolicyAndUpdateWithInsuredsPass()
+        {
+            var policyModel = new StubIPolicyModel
+            {
+                EffectiveDateGet = () => new DateTime(2014, 12, 25),
+                ExpirationDateGet = () => new DateTime(2015, 12, 25),
+                InsuredsGet = () => new List<IInsuredModel>(),
+                CoveragesGet = () => new List<ICoverageModel>(),
+                UserGet = () => "testmb"
+            };
+
+            var insuredModel = new StubIInsuredModel
+            {
+                FirstNameGet = () => "Peter",
+                LastNameGet = () => "Quill",
+                DbaNameGet = () => "Star Lord",
+                InsuredTypeGet = () => "Primary"
+            };
+
+            var service = PolicyServiceFactory.GetService(typeof (PolicyDbService));
+            var policy = service.CreatePolicy(policyModel);
+
+            policy.Insureds.Add(insuredModel);
+
+            policy = service.UpdatePolicy(policy);
+
+            Assert.IsTrue(policy.Insureds.First().InsuredId > 0);
+        }
+    }
+}
